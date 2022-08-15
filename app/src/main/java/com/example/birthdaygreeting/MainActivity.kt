4433419f80bbfea723501e8508397e07f690f978
Siddhant.Goyal <Siddhant.Goyal@ril.com>
2022-08-15 11:20:03 +0530

    Removed unsed imports

--- app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.birthdaygreeting
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.util.Log
-import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.birthdaygreeting.databinding.ActivityMainBinding
 import java.util.*
